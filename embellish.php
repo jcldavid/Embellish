81e60837e148355ae6f6ac1f650eb95cc0553bd7
apathetic012@gmail.com <Cyrus@cyrus-ed75cf1b9.(none)>
2012-03-19 22:10:27 +0800

    Added embellish class

---
+++ embellish.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * The most powerful QR Code generator
+ * QR for Quick Response
+ */
+class Embellish {
+	private $img;
+	/**
+	 * @params string - any text you would want the QRCode to have
+	 */
+	function __construct($string){
+		$config = include 'config.php';
+		
+		switch($config['source']){
+			case 'QRServer': 
+				$this->img =  'http://api.qrserver.com/v1/create-qr-code/?size='.$config['size'].'x'.$config['size'].'&data='.trim($string);
+				break;
+			default:
+				$this->img =  'https://chart.googleapis.com/chart?chs='.$config['size'].'x'.$config['size'].'&cht=qr&chl='.trim($string).'&chld=L|0&choe=UTF-8';
+		};
+				
+	}
+	/**
+	 * fetches the QR Image
+	 * @return QRImage
+	 */
+	function getIMG(){
+		return $this->img;
+	}
+}
