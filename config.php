3d4001c66ab142cf3de6dbea75cdeab7f12db9e4
apathetic012@gmail.com <Cyrus@cyrus-ed75cf1b9.(none)>
2012-03-20 04:36:54 +0800

    Add core files

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * This configuration file contains the secret ingredients to our
+ * QRCode Generator
+ */
+return array(
+	// 'QRServer' or default is 'GoogleCharts'
+	'source' => 'QRServer',
+	
+	//thumbnail width/height in pixels
+	'size' => '200',
+);
